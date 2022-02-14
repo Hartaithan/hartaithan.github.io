04b6b19fca074eab0297ddcb6cbf0635ab865838
hartaithan <hartaithan@gmail.com>
2022-02-14 15:57:56 +0900

    add unfinished variant of animation

--- src/hooks/useTitle.ts
+++ src/hooks/useTitle.ts
@@ -1,9 +1,39 @@
 import React from "react";
 
+let prevTitle: string = document.title.split(" | ")[0];
+let newTitle: string;
+
 const useTitle = (title: string) => {
+  const timer = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+  async function deleteAnimation() {
+    for (var i = 0; i < prevTitle.length; i++) {
+      newTitle = newTitle.slice(0, -1);
+      document.title = `${newTitle} | Hartaithan.`;
+      await timer(300);
+    }
+  }
+
+  async function writeAnimation() {
+    const array = Array.from(title);
+    let animateTitle = "";
+    for (var i = 0; i < array.length; i++) {
+      const e = array[i];
+      animateTitle = animateTitle + e;
+      document.title = `${animateTitle} | Hartaithan.`;
+      await timer(300);
+    }
+  }
+
+  async function runAnimation() {
+    newTitle = title;
+    await deleteAnimation();
+    await writeAnimation();
+  }
+
   React.useEffect(() => {
-    const prevTitle = document.title;
-    document.title = `${title} | Hartaithan.`;
+    runAnimation();
+
     return () => {
       document.title = prevTitle;
     };
